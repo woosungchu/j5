4bd9917ba6d0eb13d6ce1e02410a72b4b3e3da0f
woosungchu <woosungchu@gmail.com>
2020-04-29 16:18:57 +0900

    ex6

---
+++ j5ik/ex6_light_sensor.js
@@ -0,0 +1,28 @@
+var five = require("johnny-five");
+var Tessel = require("tessel-io");
+var board = new five.Board({
+  io: new Tessel()
+});
+
+board.on("ready", () => {
+  var light = new five.Light("a7");
+  var nightlight = new five.Led("b6");
+  var dimmest = 1023;
+  var brightest = 0;
+
+  light.on("change", () => {
+    var relativeValue;
+    if (light.value < dimmest) {
+      dimmest = light.value;
+    }
+    if (light.value > brightest) {
+      brightest = light.value;
+    }
+    relativeValue = five.Fn.scale(light.value, dimmest, brightest, 0, 511);
+    if (relativeValue <= 255) {
+      nightlight.brightness(255 - relativeValue);
+    } else {
+      nightlight.off();
+    }
+  });
+});
